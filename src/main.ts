dfa09a1aeaa5cc63824adbd228263704a529e413
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2025-12-19 05:59:56 +0100

    app

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,8 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   
-  await app.listen(3000)
+    await app.listen(process.env.PORT ?? 3000);
+
 
   // await app.init();
   // return app;
