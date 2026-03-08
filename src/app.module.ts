7a86d54fabdf379db5ab94631379b5b238b6afa6
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-03-08 18:19:41 +0000

    add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { ConfigModule } from '@nestjs/config';
 import {  ThrottlerModule } from '@nestjs/throttler';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -27,6 +28,7 @@ import { UsersModule } from './users/users.module';
       },
     ]),
      UsersModule,
+     AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
