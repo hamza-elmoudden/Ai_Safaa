1e4f9c3d6eeb5fe1f477d08278f6555618c89b62
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-03-11 15:49:32 +0000

    complete create handler skin profile

--- src/skinprofiles/Command/handler/create.profile.handler.ts
+++ src/skinprofiles/Command/handler/create.profile.handler.ts
@@ -2,23 +2,42 @@ import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { CreateProfileCommand } from "../impl/create.profile.command";
 import { SkinProfile } from "src/skinprofiles/Schema/skin.profile.schema";
 import { SkinprofilesService } from "src/skinprofiles/skinprofiles.service";
+import { UsersService } from "src/users/users.service";
+import { NotFound } from "@aws-sdk/client-s3";
+import { NotFoundException } from "@nestjs/common";
+import { User } from "src/users/Schema/user.schema";
 
 
 
 @CommandHandler(CreateProfileCommand)
-export class CreateProfileHandler implements ICommandHandler<CreateProfileCommand>{
+export class CreateProfileHandler implements ICommandHandler<CreateProfileCommand> {
     constructor(
-        private readonly service:SkinprofilesService
-    ){}
+        private readonly service: SkinprofilesService,
+        private readonly UsersService: UsersService
+    ) { }
 
-    async execute(command:CreateProfileCommand):Promise<string>{
-        const {user_id,skin_type,concerns,allergies,notes} = command;
-        //create a new profile using the service
-        const profile = new SkinProfile(crypto.randomUUID(),user_id,skin_type,concerns,allergies,notes,new Date(),new Date());
-        
-        //save the profile to the database
+    async execute(command: CreateProfileCommand): Promise<string> {
+        const { user_id, skin_type, concerns, allergies, notes } = command;
+
+        let user:User
+
+        try {
+            user = await this.UsersService.findOneId(user_id);
+
+            if (!user) {
+
+                throw new NotFoundException(`User with id ${user_id} not found`);
+            }
+
+        } catch (error) {
+            console.error('Error fetching user:', error);
+            throw new Error('Failed to fetch user');
+        }
+
+        const profile = new SkinProfile(crypto.randomUUID(), user.id, skin_type, concerns, allergies, notes, new Date(), new Date());
+
         let skinprofile;
-        
+
         try {
             skinprofile = await this.service.createProfile(profile);
         } catch (error) {
--- src/skinprofiles/skinprofiles.module.ts
+++ src/skinprofiles/skinprofiles.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { SkinprofilesService } from './skinprofiles.service';
 import { SkinprofilesController } from './skinprofiles.controller';
+import { CreateProfileHandler } from './Command/handler/create.profile.handler';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  providers: [SkinprofilesService],
-  controllers: [SkinprofilesController]
+  imports: [UsersModule],
+  providers: [
+    SkinprofilesService,
+    CreateProfileHandler,
+
+  ],
+  controllers: [SkinprofilesController],
+  exports:[SkinprofilesService]
 })
 export class SkinprofilesModule {}
