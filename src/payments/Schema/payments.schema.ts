8a046032e54b5af208cf60938b7e5cf807a16162
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-03-13 15:30:50 +0000

    add fu  c d f fbu to paymentsservice

--- src/payments/Schema/payments.schema.ts
+++ src/payments/Schema/payments.schema.ts
@@ -1,3 +1,16 @@
+import { payment_method, payment_status } from "generated/prisma/client";
 
 
-export class Payment {}
+export class Payment {
+    constructor(
+        public id:string,
+        public user_id:string,
+        public amount_mad:number,
+        public method:payment_method,
+        public status:payment_status,
+        public provider_ref?:string,
+        public provider_meta?:object,
+        public subscription_id?:string,
+        public paid_at?:Date
+    ){}
+}
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,4 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Payment } from './Schema/payments.schema';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+    constructor(
+        private  readonly prisma:PrismaService
+    ) {}
+
+
+
+    async createPayment(data:Payment){
+        const payment = await this.prisma.payments.create({
+            data:{
+               id:data.id,
+               user_id:data.user_id,
+               amount_mad:data.amount_mad,
+               method:data.method,
+               status:data.status,
+               provider_ref:data.provider_ref,
+               provider_meta:data.provider_meta,
+               subscription_id:data.subscription_id,
+               paid_at:data.paid_at
+            }
+        })
+        return payment;
+    }
+
+    async findPaymentById(id:string){
+        const payment = await this.prisma.payments.findUnique({
+            where:{
+                id:id
+            }
+        })
+        return payment;
+    }
+
+
+    async findPaymentsByUserId(user_id:string){
+        const payments = await this.prisma.payments.findMany({
+            where:{
+                user_id:user_id
+            }
+        })
+        return payments;
+    }
+
+     async deletePayment(id:string){
+        await this.prisma.payments.delete({
+            where:{
+                id:id
+            }
+        })
+    }
+}
