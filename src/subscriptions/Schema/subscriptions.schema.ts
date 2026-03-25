47b88967e647f06ebac47a9d994b4d4106b0b867
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-03-25 07:35:37 +0100

    change Subscription Schema treatment plans to number

--- src/subscriptions/Schema/subscriptions.schema.ts
+++ src/subscriptions/Schema/subscriptions.schema.ts
@@ -7,7 +7,7 @@ enum plan_name {
   free="free",
   premium="premium",
   vip="vip"
-}
+}  
 
 export class Subscription {
     constructor(
@@ -15,7 +15,7 @@ export class Subscription {
         public name: plan_name,
         public price_mad: number,
         public limit_photo_treatment: number,
-        public treatment_plans: boolean,
+        public treatment_plans: number,
         public product_checks: number,
         public progress_tracking: boolean,
         public vip_consultation: boolean,
--- src/subscriptions/subscriptions.service.ts
+++ src/subscriptions/subscriptions.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Subscription } from './Schema/subscriptions.schema';
-import { plan_name } from 'generated/prisma/enums';
 
 @Injectable()
 export class SubscriptionsService {
