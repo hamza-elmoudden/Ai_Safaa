4213144386407cfe7686c8df5c7a7d2c8b42fb99
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-03-28 16:34:09 +0100

    Update schema column

--- src/payments/Schema/payments.schema.ts
+++ src/payments/Schema/payments.schema.ts
@@ -4,15 +4,15 @@ import { payment_method, payment_status } from "generated/prisma/client";
 export class Payment {
     constructor(
         public id:string,
-        public user_id:string,
+        public user_id:string, 
         public amount_mad:number,
         public method:payment_method,
         public status:payment_status,
         public created_at:Date,
         public starts_at:Date,
+        public subscription_id:string,
         public provider_ref?:string,
         public provider_meta?:object,
-        public subscription_id?:string,
         public expires_at?:Date,
     ){}
 }  
