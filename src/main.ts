4d62834833b172a7c455d0845ee7bc0e0d6b3b7e
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-03-30 21:57:03 +0100

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import  cookieParser from 'cookie-parser';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
 
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-    allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: 'Content-Type, Accept,Authorization',
   });
 
   app.useGlobalPipes(
    new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true })
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(cookieParser());
+
+  await app.listen(process.env.PORT ?? 3001);
 
 
 
