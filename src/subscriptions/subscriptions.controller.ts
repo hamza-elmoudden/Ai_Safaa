c72a8d865cd605ac34b932df97bf4806de0ef70d
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-04-01 10:36:35 +0100

    add fun to free sub

--- src/subscriptions/subscriptions.controller.ts
+++ src/subscriptions/subscriptions.controller.ts
@@ -11,7 +11,7 @@ export class SubscriptionsController {
 
 
     @Get()
-    getSubscriptions() {
-        return this.subscriptionsService.getSubscriptions();
+    async getSubscriptions() {
+        return await this.subscriptionsService.FindAll();
     }
 }
--- src/subscriptions/subscriptions.service.ts
+++ src/subscriptions/subscriptions.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Subscription } from './Schema/subscriptions.schema';
+import { plan_name } from 'generated/prisma/enums';
 
 @Injectable()
 export class SubscriptionsService {
+    getSubscriptions() {
+        throw new Error('Method not implemented.');
+    }
     constructor(
         private readonly prisma: PrismaService,
     ) { }
- 
+
     mapToSubscription(data: any) {
         return new Subscription(
             data.id,
             data.name,
-            data.price_mad,              
-            data.limit_photo_treatment, 
+            data.price_mad,
+            data.limit_photo_treatment,
             data.treatment_plans,
             data.limit_photo_check,
             data.progress_tracking,
@@ -35,6 +39,20 @@ export class SubscriptionsService {
         });
 
         return data ? this.mapToSubscription(data) : null;
+    }
+
+
+    async FindName(name: plan_name): Promise<string> {
+        const subscription = await this.prisma.subscriptions.findUnique({
+            where: { name },
+            select: { id: true },
+        });
+
+        if (!subscription) {
+            throw new Error(`Subscription '${name}' not found`);
+        }
+
+        return subscription.id;
     }
 
 
