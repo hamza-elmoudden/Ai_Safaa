ac7ed32dfd91b5b9c5148c4b786a48f8ab7f4c1d
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-04-04 12:34:36 +0100

    look if the profile skin have

--- src/skinprofiles/Command/handler/create.profile.handler.ts
+++ src/skinprofiles/Command/handler/create.profile.handler.ts
@@ -4,7 +4,7 @@ import { SkinProfile } from "src/skinprofiles/Schema/skin.profile.schema";
 import { SkinprofilesService } from "src/skinprofiles/skinprofiles.service";
 import { UsersService } from "src/users/users.service";
 import { NotFound } from "@aws-sdk/client-s3";
-import { NotFoundException } from "@nestjs/common";
+import { BadRequestException, NotFoundException } from "@nestjs/common";
 import { User } from "src/users/Schema/user.schema";
 
 
@@ -19,28 +19,35 @@ export class CreateProfileHandler implements ICommandHandler<CreateProfileComman
     async execute(command: CreateProfileCommand): Promise<string> {
         const { user_id, skin_type, concerns, allergies, notes } = command;
 
-        let user:User | null
 
-        try {
-            user = await this.UsersService.findOneId(user_id);
 
-            if (!user) {
+        let user:User | null = await this.UsersService.findOneId(user_id);
 
-                throw new NotFoundException(`User with id ${user_id} not found`);
-            }
+      
+        if (!user) {
 
-        } catch (error) {
-            console.error('Error fetching user:', error);
-            throw new Error('Failed to fetch user');
+            throw new NotFoundException(`User with id ${user_id} not found`);
         }
+
+    
 
         const profile = new SkinProfile(crypto.randomUUID(), user.id, skin_type, concerns, allergies, notes, new Date(), new Date());
 
         let skinprofile;
 
         try {
+            skinprofile = await this.service.getProfileByUserId(command.user_id)
+
+            if(skinprofile){
+
+                throw new BadRequestException("Profile is exist")
+                
+            }
+
             skinprofile = await this.service.createProfile(profile);
+
         } catch (error) {
+
             console.error('Error creating skin profile:', error);
             throw new Error('Failed to create skin profile');
         }
