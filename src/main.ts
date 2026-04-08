0faa6c37f130dd452541099a0132773fcd96993c
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-04-08 15:00:47 +0100

    add proxy 1

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 import { config } from 'dotenv';
 import { ConfigService } from '@nestjs/config';
 
-
+ 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
